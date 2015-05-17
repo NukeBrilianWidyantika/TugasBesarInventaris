5d0ab762bcd55ed09e856cd6a4937c41fbdaa1ee
Roudla Tul Jannah <roudlatuljannah@ymail.com>
2015-05-17 17:09:57 +0700

    Update Tampil.java

--- TB_Inventaris/src/Inventaris/Tampil.java
+++ TB_Inventaris/src/Inventaris/Tampil.java
@@ -1,17 +1,21 @@
-package Inventaris;
+package inventaris;
 
 
-public class Tampil {
+public class tampil {
 	public static void main(String[] args){
-		IO call2 = new IO();
-		Model a;
+		IO panggil = new IO();
+		//Model a;
+		System.out.println("==============================================================");
+		System.out.println(" 			INVENTARIS KAMPUS");
+		System.out.println("==============================================================");
+		panggil.identitas();
 		
+		panggil.inputKondisi_ruang();
+		panggil.jml_kondisi_dan_posisi();
+		panggil.inputLingkungan_ruang();
+		panggil.inputKebersihan_ruang();
+		panggil.kenyamanan_ruang();
 		System.out.println("==============================================================");
-		System.out.println("                      INVENTARIS KAMPUS");
-		System.out.println("==============================================================");
-		
-		System.out.println("\n========================HASIL ANALISIS========================");
-		call2.tampil();
-		System.out.println("==============================================================");
-	}
+		}
+
 }
